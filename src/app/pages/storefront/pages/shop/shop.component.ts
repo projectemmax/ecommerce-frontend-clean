2e3e1412d927da4e80b528614cec0d37e5aa50e8
projectemmax <project.emmax@gmail.com>
2026-05-01 09:21:39 +0800

    fix description into 2 lines and add elipsis

--- src/app/pages/storefront/pages/shop/shop.component.ts
+++ src/app/pages/storefront/pages/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
