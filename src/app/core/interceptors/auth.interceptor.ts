1eb6e593a7be7a25c423f37ca7a229194d82b724
projectemmax <project.emmax@gmail.com>
2026-05-02 11:06:31 +0800

    update auth interceptor regarding session exprired to redirection of state

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -3,32 +3,64 @@ import {
   HttpInterceptor,
   HttpRequest,
   HttpHandler,
-  HttpEvent
+  HttpEvent,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { AuthService } from '@app/core/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+    private isRedirecting = false;
 
-  constructor(private auth: AuthService) {}
+    constructor(
+        private auth: AuthService,
+        private router: Router
+    ) {}
 
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
 
-    const token = this.auth.getToken();
+        const token = this.auth.getToken();
 
-    // 👇 IMPORTANT: skip login request
-    if (token && !req.url.includes('/auth/login')) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
+        // 👇 IMPORTANT: skip login request
+        if (token && !req.url.includes('/auth/login')) {
+        req = req.clone({
+            setHeaders: {
+            Authorization: `Bearer ${token}`
+            }
+        });
         }
-      });
-    }
 
-    return next.handle(req);
-  }
+        return next.handle(req).pipe(
+            catchError((error: HttpErrorResponse) => {
+
+                if (
+                    error.status === 401 && 
+                    !req.url.includes('/auth/login') &&
+                    !this.isRedirecting 
+                ) {
+                    this.isRedirecting = true;
+                    
+                    const isAdminRoute = this.router.url.startsWith('/admin');
+
+                    console.warn('Session expired');
+
+                    if (isAdminRoute) {
+                        // 🔐 ADMIN → force login
+                        this.auth.logout('login');
+                    } else {
+                        // 🛍️ STOREFRONT → soft logout (no redirect)
+                        this.auth.logout('storefront');
+                    }
+                }
+
+                return throwError(() => error);
+            })
+        );
+    }
 }
