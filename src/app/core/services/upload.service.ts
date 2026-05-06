784b32a7baf17a98f20f3dcedc32e89a47f039c3
projectemmax <project.emmax@gmail.com>
2026-05-06 08:50:52 +0800

    Apply environment for apiUrl for prod regarding image upload

--- src/app/core/services/upload.service.ts
+++ src/app/core/services/upload.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UploadService {
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    upload(file: File) {
-        const formData = new FormData();
-        formData.append('file', file);
+  upload(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
 
-        return this.http.post<any>('/api/site-config/upload', formData);
-    }
+    return this.http.post<any>(
+      `${environment.apiUrl}/site-config/upload`,
+      formData
+    );
+  }
 }
